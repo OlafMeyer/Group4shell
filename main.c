604dda290ab12020eba0c8b4ef9956f386adacb1
zapturk <ssmckeefer@sbcglobal.net>
2014-09-28 17:05:08 -0500

    input
    includes, int main, and a loop for getting input

--- main.c
+++ main.c
@@ -2,3 +2,20 @@
 // Group 4
 // Scott McKeefer, Tanner Cash, Olaf Meyer, Matt Truby
 
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main()
+{
+	char input;
+	while(scanf("%s", input) != NULL)
+	{
+		switch(string)
+		{
+
+		}
+	}
+
+	return 0;
+}
