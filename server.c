e60ced6dc9666336631b3ffaa6e14a36a4f758af
Olaf <olaflafmeyer@gmail.com>
2014-10-28 12:49:48 -0500

    added server.c

---
+++ server.c
@@ -0,0 +1,76 @@
+/* A simple server in the internet domain using TCP
+   The port number is passed as an argument */
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/types.h> 
+#include <sys/socket.h>
+#include <netinet/in.h>
+
+#include <time.h>
+
+void error(const char *msg)
+{
+    perror(msg);
+    exit(1);
+}
+
+int main(int argc, char *argv[])
+{
+     int sockfd, newsockfd, portno;
+     socklen_t clilen;
+     char buffer[256];
+     struct sockaddr_in serv_addr, cli_addr;
+     int n, i;
+     int ticket[10]; //initialize array of tickets
+     
+     srand(time(NULL));
+     for(i = 0; i < 10; i++) {
+        ticket[i] = rand() % 100000;
+        printf("Random ticket %d is %d\n", i+1, ticket[i]);
+     } //initialize random ticket numbers
+
+     if (argc < 2) {
+         fprintf(stderr,"ERROR, no port provided\n");
+         exit(1);
+     }
+     sockfd = socket(AF_INET, SOCK_STREAM, 0);
+     if (sockfd < 0) 
+        error("ERROR opening socket");
+     bzero((char *) &serv_addr, sizeof(serv_addr));
+     portno = atoi(argv[1]);
+     serv_addr.sin_family = AF_INET;
+     serv_addr.sin_addr.s_addr = INADDR_ANY;
+     serv_addr.sin_port = htons(portno);
+     if (bind(sockfd, (struct sockaddr *) &serv_addr,
+              sizeof(serv_addr)) < 0) 
+              error("ERROR on binding");
+     listen(sockfd,5);
+     clilen = sizeof(cli_addr);
+     newsockfd = accept(sockfd, 
+                 (struct sockaddr *) &cli_addr, 
+                 &clilen);
+     if (newsockfd < 0) 
+          error("ERROR on accept");
+     bzero(buffer,256);
+     n = read(newsockfd,buffer,255);
+     if (n < 0) error("ERROR reading from socket");
+     printf("Here is the message: %s\n",buffer);
+
+     if(buffer == "BUY") {
+        n = write(newsockfd, "You bought a ticket", 19);
+     }
+     else if(buffer == "CANCEL") {
+        n = write(newsockfd, "You cancelled your ticket", 25);
+     }
+     else {
+        n = write(newsockfd, "I did not understand your request", 33);
+     }
+
+     n = write(newsockfd,"I got your message",18);
+     if (n < 0) error("ERROR writing to socket");
+     close(newsockfd);
+     close(sockfd);
+     return 0; 
+}
